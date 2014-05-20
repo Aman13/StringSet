d341505acabcc549e6b5ff62c9fa6794a8344310
Aman13 <sandhu.amandeep.s@gmail.com>
2014-05-19 22:49:43 -0700

    main be jamerino

--- Main.cpp
+++ Main.cpp
@@ -10,11 +10,39 @@ void test_size()	{
 	s.insert("dog");
 	assert(s.size() == 1, "size should be 1.");
 }
+void test_union()	{
+	StringSet a;
+	StringSet b;
+	a.insert("dog");
+	a.insert("cat");
+	a.insert("cow");
+	a.insert("chicken");
+	a.insert("horse");
+	b.insert("dog");
+	b.insert("cat");
+	StringSet c = a.unions(b);
+	assert(c.size() == 5, "dog repeats and 5+2-2 = 5");
+}
+void test_intersection()	{
+	StringSet a;
+	StringSet b;
+	a.insert("dog");
+	a.insert("cat");
+	a.insert("cow");
+	a.insert("chicken");
+	a.insert("horse");
+	b.insert("dog");
+	b.insert("cat");
+	StringSet c = a.intersection(b);
+	assert(c.size() == 2, "because only 2 strings match");
+}
 
 
 int main()	{
 	try	{
 		test_size();
+		test_union();
+		test_intersection();
 	}
 	catch(const char* msg) {
 		std::cout << "error: " << msg << std::endl;
